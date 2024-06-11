e59bd68ba4cb14a1895d13d94bc2fbe0986e82e7
James J Balamuta <james.balamuta@gmail.com>
2024-06-11 00:17:26 -0700

    Doc entry

--- R/in-webr.R
+++ R/in-webr.R
@@ -1,7 +1,7 @@
 #' Detect if the version of R is built using Web Assembly (WASM)
 #' 
 #' The function checks whether the compiled version of _R_ shows an architecture
-#' that matches `"wasm32"`. If it's the case, we're likely using webR to access
+#' that matches `"emscripten"` . If it's the case, we're likely using webR to access
 #' R.
 #' 
 #' @export
