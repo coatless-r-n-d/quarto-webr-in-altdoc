3a13a3401cca7fb264c5ff197d18ea86afa795b6
James J Balamuta <james.balamuta@gmail.com>
2024-06-12 08:44:32 -0700

    Expand code documentation

--- R/residual_surrealism.R
+++ R/residual_surrealism.R
@@ -1,16 +1,30 @@
 #' Residual (Sur)Realism
 #'
-#' A dataset containing residuals from a surreal model.
+#' A dataset that yields a model with surreal residuals.
 #'
 #' @format 
 #' A data frame with 5,395 rows and 7 columns:
-#'
+#' \describe{
+#'   \item{V1}{Target variable of interest}
+#'   \item{V2}{First predictor variable}
+#'   \item{V3}{Second predictor variable}
+#'   \item{V4}{Third predictor variable}
+#'   \item{V5}{Fourth predictor variable}
+#'   \item{V6}{Fiveth predictor variable}
+#'   \item{V7}{Sixth predictor variable}
+#' }
+#' 
 #' @examples
+#' # Load the data
 #' data(residual_surrealism)
 #' 
+#' # Fit a model
 #' model = lm(V1 ~ ., residual_surrealism)
 #' 
+#' # Graph the residuals vs. fitted values
 #' plot(fitted(model),resid(model))
+#' 
+#' # Add a line at y = 0
 #' abline(0, 0, col = "red")
 #' 
 #' @source 
